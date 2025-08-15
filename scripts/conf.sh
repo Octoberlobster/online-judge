dfc6df2659ea732cf27bfcde08b10b57cdf41fdf
octoberlobster <nathanlee8189@gmail.com>
2025-08-15 09:03:21 +0800

    update

--- scripts/conf.sh
+++ scripts/conf.sh
@@ -194,3 +194,11 @@ EOF
 
 sudo nginx -t && sudo systemctl reload nginx && echo "已寫入 ${CONF_PATH}（語法 OK）。/"
 
+#!/bin/bash
+DMOJ_STATIC_PATH="${HOME}/dmoj-site/static"
+sudo apt update
+sudo apt install -y acl
+sudo setfacl -m u:www-data:x /home
+sudo setfacl -m u:www-data:x "${HOME}"
+sudo setfacl -R -m u:www-data:rx "$DMOJ_STATIC_PATH"
+sudo setfacl -R -m d:u:www-data:rx "$DMOJ_STATIC_PATH"
